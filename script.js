4ed66cfb430ee674595eaa45f85f684c3083ffb1
Felra13 <barbazaraphael@gmail.com>
2024-09-17 15:30:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ var imageUrl = 'CarteRouge14.jpg'; // Assure-toi que le nom et l'extension sont 
 
 // Limites de l'image en pixels (614x1024)
 var imageBounds = [[0, 0], [614, 1024]];
+
+
+// Ajouter l'image en tant que calque
+L.imageOverlay(imageUrl, imageBounds).addTo(map);
+
+// Ajuster la vue pour correspondre à l'image
+map.fitBounds(imageBounds);
