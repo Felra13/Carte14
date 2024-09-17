db98e42960ed199872b0870367f95df21be71b87
Felra13 <barbazaraphael@gmail.com>
2024-09-17 14:52:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([48.8323, 2.3195], 14);
+var map = L.map('map').setView([48.8323, 2.3195], 15);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
