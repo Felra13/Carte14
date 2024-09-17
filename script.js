22b78753fc4ea0522b7622be7ed8f670289722ec
Felra13 <barbazaraphael@gmail.com>
2024-09-17 15:27:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-// script.js
+// Initialise la carte avec un système de coordonnées simple
 var map = L.map('map', {
     crs: L.CRS.Simple, // Utilise un système de coordonnées simple pour les images
     maxZoom: 5
 });
 
-// URL de l'image
-var imageUrl = 'CarteRouge14.jpg'; // Assure-toi d'utiliser le nom et l'extension corrects
+// URL de l'image de la carte
+var imageUrl = 'CarteRouge14.jpg'; // Assure-toi que le nom et l'extension sont corrects
 
-// Définir les limites de l'image en pixels
-var imageBounds = [[0, 0], [614, 1024]]; // Ajuste ces valeurs selon les dimensions de l'image (614x1024)
+// Limites de l'image en pixels (614x1024)
+var imageBounds = [[0, 0], [614, 1024]];
 
 // Ajouter l'image en tant que calque
 L.imageOverlay(imageUrl, imageBounds).addTo(map);
 
 // Ajuster la vue pour correspondre à l'image
 map.fitBounds(imageBounds);
-
