051ec1cf40ae518d39b23b38f97b6dd25f47d1a2
Felra13 <barbazaraphael@gmail.com>
2024-09-17 15:14:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var map = L.map('map', {
 
 // URL de l'image
 var imageUrl = 'CarteRouge14.jpg'; // Remplace par le nom exact de ton fichier
-var imageBounds = [[0, 0], [1024, 614]]; // Ajuste ces valeurs en fonction des dimensions de l'image
+var imageBounds = [[0, 0], [614, 1024]]; // Ajuste ces valeurs en fonction des dimensions de l'image
 
 L.imageOverlay(imageUrl, imageBounds).addTo(map);
 map.fitBounds(imageBounds);
